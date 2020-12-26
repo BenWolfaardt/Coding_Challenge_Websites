ea584ade3d97d5f590408b9d16fabb1deb3688a8
Ben Wolfaardt <35372441+BenWolfaardt@users.noreply.github.com>
2020-12-26 19:36:08 +0100

    Update Day08_Dictionaries_and_maps.cpp

--- C++/30_Days_of_code/Day08_Dictionaries_and_maps.cpp
+++ C++/30_Days_of_code/Day08_Dictionaries_and_maps.cpp
@@ -4,4 +4,5 @@ me being a skollie
 10/12/2020 - lol its been 2 months now
 15/12/2020 - trollolol
 17/12/2020 - information overload with Jason on all tech stacks webdev, took notes and Sunny coded
-21/12/2020 - Setting up Python and C++ on work laptop*/
+21/12/2020 - Setting up Python and C++ on work laptop
+26/12/2020 - LaTeX CV and FourX meeting*/

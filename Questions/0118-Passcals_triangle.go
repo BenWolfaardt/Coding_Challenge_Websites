f318f405c06edf907fc0129bb321a7cbd653dd2a
BenWolfaardt <benjaminwolfaardt@gmail.com>
2022-01-26 21:34:01 +0100

    completed

--- Questions/0118-Passcals_triangle.go
+++ Questions/0118-Passcals_triangle.go
@@ -10,6 +10,23 @@ func generate(numRows int) [][]int {
 	defer elapsedTime.Timer(time.Now())
 
 	var triangle [][]int
+	var prevRow = []int{1}
 
+	if numRows > 0 {
+		triangle = append(triangle, prevRow)
+	}
+
+	for i := 1; i < numRows; i++ {
+		lenPrevRow := len(prevRow)
+		var currentRow = make([]int, lenPrevRow+1)
+
+		currentRow[0], currentRow[lenPrevRow] = 1, 1
+
+		for j := 1; j < lenPrevRow; j++ {
+			currentRow[j] = prevRow[j-1] + prevRow[j]
+		}
+		triangle = append(triangle, currentRow)
+		prevRow = currentRow
+	}
 	return triangle
 }
--- Questions/0118-Passcals_triangle_test.go
+++ Questions/0118-Passcals_triangle_test.go
@@ -1,18 +1,52 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test(t *testing.T) {
+func Test_generate(t *testing.T) {
+	type args struct {
+		num_rows int
+	}
 	testCases := []struct {
-		desc string
+		name string
+		args args
+		want [][]int
 	}{
 		{
-			desc: "",
+			name: "Example 1",
+			args: args{num_rows: 5},
+			want: [][]int{
+				{1},
+				{1, 1},
+				{1, 2, 1},
+				{1, 3, 3, 1},
+				{1, 4, 6, 4, 1},
+			},
+		},
+		{
+			name: "Example 2",
+			args: args{num_rows: 10},
+			want: [][]int{
+				{1},
+				{1, 1},
+				{1, 2, 1},
+				{1, 3, 3, 1},
+				{1, 4, 6, 4, 1},
+				{1, 5, 10, 10, 5, 1},
+				{1, 6, 15, 20, 15, 6, 1},
+				{1, 7, 21, 35, 35, 21, 7, 1},
+				{1, 8, 28, 56, 70, 56, 28, 8, 1},
+				{1, 9, 36, 84, 126, 126, 84, 36, 9, 1},
+			},
 		},
 	}
 	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-
+		t.Run(tC.name, func(t *testing.T) {
+			if got := generate(tC.args.num_rows); !reflect.DeepEqual(got, tC.want) {
+				t.Errorf("generate() = %v, want %v", got, tC.want)
+			}
 		})
 	}
 }

3c01c3722a0a623d7ac89426c5b963920a79118b
BenWolfaardt <benjaminwolfaardt@gmail.com>
2022-01-26 20:35:41 +0100

    initial commit

---
+++ Questions/118-Passcals_triangle.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import (
+	elapsedTime "BenWolfaardt/Practice_Coding_Challenges/tree/02-LeetCode-Go/Utilities"
+	"time"
+)
+
+func generate(numRows int) [][]int {
+	// initiate timer for function
+	defer elapsedTime.Timer(time.Now())
+
+	var triangle [][]int
+
+	return triangle
+}
